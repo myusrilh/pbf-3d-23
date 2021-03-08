bceb8de6cc214072246552c605be037ea949d3e8
myusrilh <yusrilhasriansyah@gmail.com>
2021-03-08 12:22:59 +0700

    update markdown 04

---
+++ src/04_konsep_reactjs_part2/praktikum2/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function SambutanUser (props) {
+  return <h1>Selamat Datang 1841720184 - Muhammad Yusril Hasriansyah!</h1>;
+}
+
+function SambutanTamu (props) {
+  return <h1>Mohon melakukan sign up terlebih dahulu.</h1>;
+}
+
+function Sambutan (props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <SambutanUser />;
+  }
+  return <SambutanTamu />;
+}
+
+ReactDOM.render(
+  // Silakan coba ganti nilai isLoggedIn={false}
+  <Sambutan isLoggedIn={false} />,
+  document.getElementById('root')
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/04_konsep_reactjs_part2/praktikum5/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function Blog (props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  { id: 1, title: 'Biodata Saya', content: 'NIM: 1841720184 - Nama: Muhammad Yusril Hasriansyah' },
+  { id: 2, title: 'Alamat Rumah', content: 'Alamat rumah saya di Jl Brawijaya Kota Gresik' }
+];
+ReactDOM.render(
+  <Blog posts={posts} />,
+  document.getElementById('root')
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
