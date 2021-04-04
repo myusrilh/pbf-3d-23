563bb2e7dc5de4a7759a3de3f482ccc4791260c1
myusrilh <yusrilhasriansyah@gmail.com>
2021-04-04 18:32:16 +0700

    add 07_context_hoc

---
+++ src/07_context_hoc/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+// import './App.css';
+import ThemeProvider from "./ThemeContext/ThemeProvider";
+import Menu from "./Menu";
+import SetColor from "./SetColor";
+
+export default function App () {
+  return (
+    <ThemeProvider>
+      <Menu />
+      <br />
+      <SetColor />
+    </ThemeProvider>
+  )
+}
+
+// export default App;
